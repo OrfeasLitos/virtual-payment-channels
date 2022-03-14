6b06571be38e4f4cbd854de29a6818e74b50c178
Orfeas Litos <orfeas.litos@hotmail.com>
2022-03-14 22:12:37 +0100

    Apply yapf to Simulation.py

--- simulation/Simulation.py
+++ simulation/Simulation.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractmethod
 
-Bitcoin_money_const = 1                 # Should probably be modified
-Bitcoin_time_const = 3600               # 1h = 3600 seconds
+Bitcoin_money_const = 1  # Should probably be modified
+Bitcoin_time_const = 3600  # 1h = 3600 seconds
+
 
 # maybe Network should extend a class "LabeledGraph"
 class Network:
-    def __init__(self, vertices = None, edges = None, nr_vertices = None):
+
+    def __init__(self, vertices=None, edges=None, nr_vertices=None):
         """
         vertices should be a list.
         edges is a list of tuples containing exactly two elements. The first element specifies what kind of channel is used, i.e Bitcoin, Lightning, Elmo,...
@@ -16,20 +18,20 @@ class Network:
             self.edges = []
         if (vertices != None):
             self.vertices = vertices
-        if (edges != None):    
+        if (edges != None):
             self.edges = edges
-        
+
     def add_vertex(self, vertex):
         self.vertices.append(vertex)
 
     def add_edge(self, edge):
         self.edges.append(edge)
 
-
     # remove vertices, edges ...
-    
+
 
 class Knowledge:
+
     def __init__(self, party, knowledge_function):
         """
         Knowledge could be a list of payments or a probability distribution.
@@ -37,7 +39,7 @@ class Knowledge:
         For beginning just take the complete list of payments.
         """
         #self.knowledge = fct(party, lst_payments)  # fct has to be specified
-        # or 
+        # or
         self.knowledge = knowledge_function
         self.party = party
 
@@ -46,11 +48,13 @@ class Knowledge:
 
     # maybe an update method
 
+
 class Payment:
     """
     In this class the payment method, sender, receiver and content are specified.
     It is possible to get the cost of a transaction.
     """
+
     def __init__(self, payment_method, sender, receiver, content):
         self.payment_method = payment_method
         self.content = content
@@ -60,12 +64,13 @@ class Payment:
         self.size = 5
 
     #def get_payment_size(self):
-        # to be done
+    # to be done
 
     def get_transaction_cost(self):
-        unit_money_cost, unit_time_cost = self.payment_method.get_unit_transaction_cost()
+        unit_money_cost, unit_time_cost = self.payment_method.get_unit_transaction_cost(
+        )
         return (self.size * unit_money_cost, unit_time_cost)
-    
+
 
 class PaymentMethod:
     """
@@ -77,18 +82,24 @@ class PaymentMethod:
     def get_unit_transaction_cost(self):
         pass
 
+
 class PlainBitcoin(PaymentMethod):
+
     def __init__(self):
         super(PlainBitcoin, self).__init__()
-    def get_unit_transaction_cost(self):   
+
+    def get_unit_transaction_cost(self):
         return (Bitcoin_money_const, Bitcoin_time_const)
 
+
 class Utility:
+
     def __init__(self, function):
         self.function = function
 
     def evaluate(self, parameters):
         return self.function(parameters)
+
 
 class Simulation:
     """
@@ -97,7 +108,9 @@ class Simulation:
     There's an update method which is one step in the simulation.
     Running the simulation is equivalent to calling the update method as long as it's possible.
     """
-    def __init__(self, nr_players, payment_lst, knowledge, utility_fct, payment_method):
+
+    def __init__(self, nr_players, payment_lst, knowledge, utility_fct,
+                 payment_method):
         self.nr_players = nr_players
         self.network = Network(None, None, self.nr_players)
         print(payment_lst)
@@ -105,8 +118,8 @@ class Simulation:
         self.payments_iterator = iter(payment_lst)
         self.knowledge = knowledge
         self.utility_fct = utility_fct
-        self.payment_method = payment_method    # probably not necessary here
-    
+        self.payment_method = payment_method  # probably not necessary here
+
     def update(self):
         payment = next(self.payments_iterator, None)
         if payment == None:
@@ -123,14 +136,18 @@ class Simulation:
         while update_possible:
             update_possible = self.update()
         return
+
     # Should Simulation extend Iterator?
-    
-        
+
+
 if __name__ == "__main__":
+
     def identity_fct(x):
         return x
+
     def identity_2(*args):
         return args
+
     utility = Utility(identity_fct)
     print(utility.evaluate(5))
     bitcoin = PlainBitcoin()
@@ -140,7 +157,7 @@ if __name__ == "__main__":
     payment2 = Payment(bitcoin, 0, 4, "bar")
     network = Network(None, None, 5)
     print(network.vertices, network.edges)
-    edge = (bitcoin, ((1,2)))
+    edge = (bitcoin, ((1, 2)))
     network.add_edge(edge)
     print(network.vertices, network.edges)
     knowledge = Knowledge(1, identity_2)
