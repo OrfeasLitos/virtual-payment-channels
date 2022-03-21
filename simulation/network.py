2ff6d7432667f15f58a9ad8ed835e5a0dfb01a66
Orfeas Litos <orfeas.litos@hotmail.com>
2022-03-21 22:06:21 +0100

    Store edges in set(), simplify relevant functions

--- simulation/network.py
+++ simulation/network.py
@@ -4,26 +4,15 @@ class Network:
 
     def __init__(self, nr_vertices):
         self.vertices = list(range(nr_vertices))
-        self.edges = []
+        self.edges = set()
 
     def add_vertex(self, vertex):
         self.vertices.append(vertex)
 
     def add_edge(self, edge):
-        self.edges.append(edge)
+        self.edges.add(edge)
 
-    def get_edge(self, edge):
-        for e in self.edges:
-            # if the first part of edge is sth like "PlainBitcoin" it's ok, if it is a Bitcoin object I would need to overwrite the __eq__ method.
-            if e == edge:
-                return e
-        return None
-    
-    def add_edge_with_check(self, edge):
-        """
-        This method only adds an edge if it doesn't exist yet.
-        """
-        if self.get_edge(edge) == None:
-            self.add_edge(edge)
+    def has_edge(self, edge):
+        return edge in self.edges
 
     # TODO: remove vertices, edges ...
