9095d0bd23fa44ee2015525c00d5d06975bac0e4
uscpr <michael.schaller@tutanota.de>
2022-03-27 21:35:42 +0200

    Add method for adjacency matrix in network

--- simulation/network.py
+++ simulation/network.py
@@ -1,4 +1,5 @@
 # maybe Network should extend a class "LabeledGraph"
+import numpy as np
 
 class Network:
 
@@ -14,6 +15,16 @@ class Network:
 
     def has_edge(self, edge):
         return edge in self.edges
+
+    def get_adjacency_matrix(self):
+        # edge is just a set of vertices
+        nr_vertices = len(self.vertices)
+        adjacency_matrix = np.zeros((nr_vertices, nr_vertices))
+        for vertex1 in self.vertices:
+            for vertex2 in self.vertices:
+                if set(vertex1, vertex2) in self.edges:
+                    adjacency_matrix[vertex1, vertex2] = 1
+        return adjacency_matrix
 
     def __eq__(self, other):
         return (self.edges == other.edges and self.vertices == other.vertices)
