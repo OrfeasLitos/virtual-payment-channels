5845ec7ba87269ce88a2c402b7b592a88c09a310
uscpr <michael.schaller@tutanota.de>
2022-04-04 21:01:51 +0200

    Write find_all_paths for multigraph

--- simulation/network.py
+++ simulation/network.py
@@ -18,24 +18,40 @@ class Network:
         self.edge_id += 1
         
     # TODO: find library function that does this for us
-    def find_all_paths(self, start, end, amount, path = []):
+    def find_all_paths(self, start, end, amount, path = [], edge_ids = []):
+        # networkX and jgrapht are the only python libs I found that support multigraphs
+        # I didn't find any function that does exactly what we want (with the cutoff for the amount, most search either shortest path or all (simple) paths)
+        # nx all_simple_paths is inconvenient as it gives only the nodes of the paths not the edges of the multigraph
+        # all_paths = nx.all_simple_paths(self.multigraph, start, end)
+
+
         # TODO: use adjacency_list or simply directly self.edges (2nd option slower)
-        adjacency_matrix = self.get_weighted_adjacency_matrix()
-        # or self.adjacency_matrix
-        nr_vertices = len(self.vertices)
+        vertices = self.multigraph.nodes()
+        nr_vertices = len(vertices)
         path = path + [start]
         if start == end:
-            return set(path)
+            # no set anymore as it would give a type error for tuples
+            return [(path, edge_ids)]
         # TODO: remove the next if, if we're too slow
-        if start not in self.vertices:
+        if start not in vertices:
             raise ValueError("start not in vertices")
-        paths = set()
+        paths = []
         # TODO: use adjacency_list
-        for vertex in range(nr_vertices):
-            if adjacency_matrix[start, vertex] >= amount and vertex not in path:
-                newpaths = self.find_all_paths(vertex, end, amount, path)
-                for newpath in newpaths:
-                    paths.add(newpath)
+        for vertex in vertices:
+            try:
+                # this gives a dict which has as many elements (dicts again) as the number of channels from start to vertex.
+                # In these dicts is the edge_id and the balance of the sender
+                edges_from_start_to_vertex = self.multigraph[start][vertex]
+                for edge_nr in edges_from_start_to_vertex.keys():
+                    edge_id = edges_from_start_to_vertex[edge_nr]['edge_id']
+                    # We check whether the balance is high enough and we want only simple paths.
+                    if edges_from_start_to_vertex[edge_nr]['balanceSender'] >= amount and vertex not in path:
+                        newpaths = self.find_all_paths(vertex, end, amount, path, edge_ids + [edge_id])
+                        for i in range(len(newpaths)):
+                            newpath, new_edge_ids = newpaths[i]
+                            paths.append((newpath, new_edge_ids))
+            except KeyError:
+                continue
         return paths
 
     def __eq__(self, other):
