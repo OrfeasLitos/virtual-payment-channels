19e4c883a524184f84fc892c88fa9d5759f1dc74
uscpr <michael.schaller@tutanota.de>
2022-04-04 17:55:06 +0200

    Remove has_edge and get_adjacency_list methods

--- simulation/network.py
+++ simulation/network.py
@@ -8,7 +8,6 @@ class Network:
         self.multigraph = nx.empty_graph(nr_vertices, create_using=nx.MultiDiGraph)
         self.edge_id = 0
 
-
     def add_node(self, node):
         self.multigraph.add_node(node)
         # do we need this method? If we need it we need sth like a convention that a new vertex gets the number len(self.vertices).
@@ -18,22 +17,6 @@ class Network:
         self.multigraph.add_weighted_edges_from([(idB, idA, balB)], weight = "balanceSender", edge_id = self.edge_id)
         self.edge_id += 1
         
-
-    def has_edge(self, edge):
-        return edge in self.edges
-
-    def get_weighted_adjacency_list(self):
-        # the i-th element of the adjacency list is the list of vertices adjacent to vertex i. Could also be a dict or deque.
-        # Maybe it would be convenient to be able to access all edges going from A to B at once, i.e to have sth like adjacency_list[idA][idB] to be a list of tuples (edge_id, balA).
-        adjacency_list = [[] for i in range(len(self.vertices))]
-        for edge in self.edges:
-            edge_id, ids_parties, balA = edge
-            idA, idB = ids_parties
-            adjacency_list[idA].append((idB, edge_id, balA))
-        return adjacency_list
-
-    # following the code from the following link (Dijkstra)
-    # https://stackoverflow.com/questions/24471136/how-to-find-all-paths-between-two-graph-nodes
     # TODO: find library function that does this for us
     def find_all_paths(self, start, end, amount, path = []):
         # TODO: use adjacency_list or simply directly self.edges (2nd option slower)
