39fa90b1515448ccfe0062b00b6d1d96ff24d06f
uscpr <michael.schaller@tutanota.de>
2022-04-04 21:15:12 +0200

    Test find_all_paths method

--- simulation/tests.py
+++ simulation/tests.py
@@ -9,49 +9,34 @@ from network import Network
 import random
 import sys
 
-def get_weighted_adjacency_matrix(network):
-    # edge is just a set of vertices
-    nr_vertices = len(network.vertices)
-    weighted_adjacency_matrix = np.zeros((nr_vertices, nr_vertices))
-    money_on_network = 0
-    for vertex1 in network.vertices:
-        for vertex2 in network.vertices:
-            for edge in network.edges:
-                parties, balance_a, balance_b = edge
-                a , b = parties
-                if (a == vertex1 and b == vertex2) or (a == vertex2 and b == vertex1):
-                    weighted_adjacency_matrix[vertex1, vertex2] = balance_a + balance_b
-                    money_on_network += balance_a + balance_b
-    for vertex1 in network.vertices:
-        for vertex2 in network.vertices:
-            weighted_adjacency_matrix[vertex1, vertex2] /= money_on_network
-    return weighted_adjacency_matrix
 
-def test_adjacency_matrix():
-    # TODO: use adjacency list
-    network = Network(5)
-    network.add_channel(0, 1, 2, 0)
-    network.add_channel(1, 1.2, 2, 5.6)
-    network.add_channel(3, 0.1, 4, 3.2)
-    network.add_channel(1, 1, 0, 2)
-    adjacency_matrix_edges = network.get_weighted_adjacency_matrix()
-    adjacency_matrix_vertices = get_weighted_adjacency_matrix(network)
-    return (np.linalg.norm(adjacency_matrix_edges - adjacency_matrix_vertices) < 10**(-10))
 
 def test_find_paths():
     # TODO: use adjacency list
-    network = Network(5)
-    edge1 = (set({0,2}, 1, 0))
-    edge2 = (set({1,2}, 1.2, 5.6))
-    edge3 = (set({3,4}, 0.1, 3.2))
-    edge4 = (set({1,0}, 1, 2))
-    network.add_edge(edge1)
-    network.add_edge(edge2)
-    network.add_edge(edge3)
-    network.add_edge(edge4)
-    paths_calculated = network.find_all_paths(0, 2)
-    paths = set({[0,2], [0,1,2]})
-    return paths_calculated == paths
+    network = Network(10)
+    network.add_channel(2, 3.4, 6, 6.7)
+    network.add_channel(2, 4., 5, 5.6)
+    network.add_channel(5, 3.2, 6, 7.8)
+    network.add_channel(8, 5.4, 6, 3.4)
+    network.add_channel(3, 3., 4, 3.2)
+    network.add_channel(2, 5.6, 3, 5.)
+    network.add_channel(4, 7.8, 6, 3.9)
+    all_paths = network.find_all_paths(2, 6, 0)
+    paths = set()
+    edge_ids = set()
+    for i in range(3):
+        path, edge_id = all_paths[i]
+        paths.add(tuple(path))
+        edge_ids.add(tuple(edge_id))
+    right_paths = set()
+    right_paths.add(tuple([2, 6]))
+    right_paths.add(tuple([2,5,6]))
+    right_paths.add(tuple([2,3,4,6]))
+    right_edge_ids = set()
+    right_edge_ids.add(tuple([0]))
+    right_edge_ids.add(tuple([1,2]))
+    right_edge_ids.add(tuple([5,4,6]))
+    return right_paths == paths and right_edge_ids == edge_ids
 
 def is_deterministic():
 
@@ -89,5 +74,7 @@ def is_deterministic():
 if __name__ == "__main__":
     #assert(is_deterministic())
     #assert(test_adjacency_matrix())
-    #assert(test_find_paths())
+    assert(test_find_paths())
     print("Success")
+
+
