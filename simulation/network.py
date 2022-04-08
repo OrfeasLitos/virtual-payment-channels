245754b21d2722730f356e63903b86ed22e679f6
uscpr <michael.schaller@tutanota.de>
2022-04-08 18:52:20 +0200

    change attribute multigraph to graph

--- simulation/network.py
+++ simulation/network.py
@@ -8,27 +8,25 @@ UNIT_COST = 1
 class Network:
 
     def __init__(self, nr_vertices):
-        # TODO: change multigraph to graph
-        self.multigraph = nx.empty_graph(nr_vertices, create_using=nx.DiGraph)
+        self.graph = nx.empty_graph(nr_vertices, create_using=nx.DiGraph)
         self.edge_id = 0
 
     def add_node(self, node):
-        self.multigraph.add_node(node)
+        self.graph.add_node(node)
         # do we need this method? If we need it we need sth like a convention that a new vertex gets the number len(self.vertices).
 
     def add_channel(self, idA, balA, idB, balB):
         assert(balA > 0 or balB > 0)
         edges = [(idA, idB, dict({'balance': balA, 'cost' : UNIT_COST})),(idB, idA, dict({'balance': balB, 'cost' : UNIT_COST}))]
-        self.multigraph.add_edges_from(edges)
+        self.graph.add_edges_from(edges)
         self.edge_id += 1
     
     def find_cheapest_path(self, start, end, amount):
-        self.multigraph.nodes[start]['amount'] = -amount
-        self.multigraph.nodes[end]['amount'] = amount
-        cost, path = nx.network_simplex(self.multigraph,
-        demand='amount', capacity='balance', weight='cost')
-        self.multigraph.nodes[start]['amount'] = 0
-        self.multigraph.nodes[end]['amount'] = 0
+        self.graph.nodes[start]['amount'] = -amount
+        self.graph.nodes[end]['amount'] = amount
+        cost, path = nx.network_simplex(self.graph, demand='amount', capacity='balance', weight='cost')
+        self.graph.nodes[start]['amount'] = 0
+        self.graph.nodes[end]['amount'] = 0
         # cost should be number of edges - 1
         return cost - 1, path
 
