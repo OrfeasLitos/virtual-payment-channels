cdddc5793fdfb248c6ed9751ed550eaa6a19eb2b
uscpr <michael.schaller@tutanota.de>
2022-05-21 19:58:27 +0200

    own method get_new_channel_option

--- simulation/paymentmethod.py
+++ simulation/paymentmethod.py
@@ -125,6 +125,37 @@ class LN(PlainBitcoin):
         }
         return onchain_option
 
+    def get_new_channel_option(self, sender, receiver, value, future_payments, counterparty):
+        new_channel_time = self.plain_bitcoin.get_delay() + self.ln_delay
+        new_channel_fee = self.plain_bitcoin.get_fee() * self.opening_transaction_size
+        # TODO: incorporate counterparty in min_amount
+        min_amount = self.sum_future_payments_to_receiver(receiver, future_payments)
+        # review: give receiver the current payment value (corresponds to `push_msat` of LN).
+        # review: our initial coins should be slightly higher than the minimum needed,
+        # review: in order to accommodate for future payments and act as intermediary.
+        # review: we can say e.g. `min(our on-chain coins, 2 * (min_amount - value))` and we can improve from there
+        self.network.add_channel(sender, min_amount, receiver, min_amount)
+        new_channel_centrality = self.network.get_harmonic_centrality()
+        new_channel_distance = self.distance_to_future_parties(future_payments)
+        # TODO: adjust future_payments.
+        new_channel_offchain_option = self.get_offchain_option(
+            sender, receiver, value, future_payments[1:])
+        self.network.close_channel(sender, receiver)
+        # TODO: make a loop that gives us several possible new channels with different counterparties
+        counterparty = receiver
+        # TODO: adjust onchain coins of sender
+        sender_coins = min(self.plain_bitcoin.coins[sender] , 2 * min_amount - value)
+        new_channel_option = {
+            'delay': new_channel_time,
+            'fee': new_channel_fee,
+            'centrality': new_channel_centrality,
+            'distance': new_channel_distance,
+            # TODO: new_channel_offchain_option empty if counterparty is receiver
+            'payment_information': { 'kind': 'ln-open', 'data': (
+                sender, receiver, value, counterparty, sender_coins, new_channel_offchain_option) }
+        }
+        return new_channel_option
+
     def get_offchain_option(self, sender, receiver, value, future_payments):
         # TODO: check if there's a better method to say that there is no path than to return None as offchain_option
         offchain_option = None
@@ -153,37 +184,9 @@ class LN(PlainBitcoin):
 
         onchain_option = self.get_onchain_option(sender, receiver, value, future_payments)
         # review: consider trying out opening other channels as well, e.g. a channel with the party that appears most often (possibly weighted by coins) in our future
-        # maybe make methods for new_channel time and fee as well
-        new_channel_time = self.plain_bitcoin.get_delay() + self.ln_delay
-        new_channel_fee = self.plain_bitcoin.get_fee() * self.opening_transaction_size
-        # TODO: incorporate counterparty in min_amount
-        min_amount = self.sum_future_payments_to_receiver(receiver, future_payments)
-        # receiver doesn't need same minimum amount, what should he put on channel?
-        # review: give receiver the current payment value (corresponds to `push_msat` of LN).
-        # review: our initial coins should be slightly higher than the minimum needed,
-        # review: in order to accommodate for future payments and act as intermediary.
-        # review: we can say e.g. `min(our on-chain coins, 2 * (min_amount - value))` and we can improve from there
-        self.network.add_channel(sender, min_amount, receiver, min_amount)
-        new_channel_centrality = self.network.get_harmonic_centrality()
-        new_channel_distance = self.distance_to_future_parties(future_payments)
-        # TODO: adjust future_payments.
-        new_channel_offchain_option = self.get_offchain_option(
-            sender, receiver, value, future_payments[1:])
-        self.network.close_channel(sender, receiver)
-        # TODO: make a loop that gives us several possible new channels with different counterparties
-        counterparty = receiver
-        sender_coins = min(self.plain_bitcoin.coins[sender] , 2 * min_amount - value)
-        new_channel_option = {
-            'delay': new_channel_time,
-            'fee': new_channel_fee,
-            'centrality': new_channel_centrality,
-            'distance': new_channel_distance,
-            # TODO: check if counterparty_coins is needed
-            # TODO: new_channel_offchain_option empty if counterparty is receiver
-            'payment_information': { 'kind': 'ln-open', 'data': (
-                sender, receiver, value, counterparty, sender_coins, new_channel_offchain_option) }
-        }
 
+        counterparty = receiver
+        new_channel_option = self.get_new_channel_option(sender, receiver, value, future_payments, counterparty)
         # TODO: check if there's a better method to say that there is no path than to return None as offchain_option
         offchain_option = self.get_offchain_option(sender, receiver, value, future_payments)
         # TODO: list shouldn't be fixed length
