c706838d47776ae5fcdfb0bee20ac530eca9c018
uscpr <michael.schaller@tutanota.de>
2022-06-10 12:43:41 +0200

    use seed in the right place in test

--- simulation/tests.py
+++ simulation/tests.py
@@ -28,14 +28,12 @@ def test_random_payments_seed_inside():
     assert random_payments1 == random_payments_seed_inside2
     assert random_payments_seed_inside1 == random_payments_seed_inside2
 
-random.seed(5)
-
 def test_random_payments_seed_outside():
     random_payments_seed_outside = random_payments(100, 10, 2000000000)
     random.seed(5)
     random_payments_seed_inside = random_payments(100, 10, 2000000000)
     assert random_payments1 == random_payments_seed_inside
-    assert random_payments_seed_inside != random_payments_seed_outside
+    assert random_payments_seed_inside == random_payments_seed_outside
 
 def make_example_network(base_fee = 1000, ln_fee = 0.00002):
     lightning = LN(10, base_fee = base_fee, ln_fee = ln_fee)
@@ -469,6 +467,7 @@ if __name__ == "__main__":
     test_choose_payment_method()
     test_simulation_with_ln()
     test_random_payments_seed_inside()
+    random.seed(5)
     test_random_payments_seed_outside()
     print("Success")
 
