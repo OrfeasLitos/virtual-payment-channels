ffd929ab49c9e9a7891c738fef6e24e72ac10058
uscpr <michael.schaller@tutanota.de>
2022-07-11 15:13:01 +0200

    unify two almost equal tests

--- simulation/tests_elmo.py
+++ simulation/tests_elmo.py
@@ -395,7 +395,7 @@ def test_coop_close_channel_first_virtual_layer_no_layer_above():
     assert elmo.network.graph[4][1]['channels_above'] == []
     assert elmo.network.graph[1][4]['channels_above'] == []
 
-def test_coop_close_channel_first_virtual_layer_one_layer_above_first_direction():
+def test_coop_close_channel_first_virtual_layer_one_layer_above(forward = True):
     elmo = make_example_network_elmo()
     future_payments = [(0,1,2000000000.), (0, 7, 1500000000.), (0,7,2100000000.), (0, 8, 300000000.), (0, 3, 2500000000.)]
     value1 = 100000000
@@ -423,35 +423,10 @@ def test_coop_close_channel_first_virtual_layer_one_layer_above_first_direction(
     assert elmo.network.graph[3][0]['channels_above'] == [{0, 8}]
     assert elmo.network.graph[3][8]['channels_above'] == [{0, 8}]
     assert elmo.network.graph[8][3]['channels_above'] == [{0, 8}]
-    elmo.network.cooperative_close_channel(0, 3)
-    assert elmo.network.graph[0][8]['channels_below'] == [0, 2, 3, 8]
-    assert elmo.network.graph[8][0]['channels_below'] == [8, 3, 2, 0]
-    assert elmo.network.graph[0][2]['channels_above'] == [{0, 8}]
-    assert elmo.network.graph[2][0]['channels_above'] == [{0, 8}]
-    assert elmo.network.graph[3][2]['channels_above'] == [{0, 8}]
-    assert elmo.network.graph[2][3]['channels_above'] == [{0, 8}]
-    assert elmo.network.graph[3][8]['channels_above'] == [{0, 8}]
-    assert elmo.network.graph[8][3]['channels_above'] == [{0, 8}]
-    assert elmo.network.graph.get_edge_data(0, 3) is None
-    assert elmo.network.graph.get_edge_data(3, 0) is None
-
-def test_coop_close_channel_first_virtual_layer_one_layer_above_second_direction():
-    elmo = make_example_network_elmo()
-    future_payments = [(0,1,2000000000.), (0, 7, 1500000000.), (0,7,2100000000.), (0, 8, 300000000.), (0, 3, 2500000000.)]
-    value1 = 100000000
-    payment_options1 = elmo.get_payment_options(0, 3, value1, future_payments)
-    assert payment_options1[2]['payment_information']['kind'] == 'Elmo-open-virtual-channel'
-    payment_information_new_virtual_channel1 = payment_options1[2]['payment_information']
-
-    elmo.do(payment_information_new_virtual_channel1)
-
-    value2 = 1000000
-    payment_options2 = elmo.get_payment_options(0, 8, value2, future_payments)
-    assert payment_options2[2]['payment_information']['kind'] == 'Elmo-open-virtual-channel'
-    payment_information_new_virtual_channel2 = payment_options2[2]['payment_information']
-
-    elmo.do(payment_information_new_virtual_channel2)
-    elmo.network.cooperative_close_channel(3, 0)
+    if forward == True:
+        elmo.network.cooperative_close_channel(0, 3)
+    else:
+        elmo.network.cooperative_close_channel(3, 0)
     assert elmo.network.graph[0][8]['channels_below'] == [0, 2, 3, 8]
     assert elmo.network.graph[8][0]['channels_below'] == [8, 3, 2, 0]
     assert elmo.network.graph[0][2]['channels_above'] == [{0, 8}]
@@ -481,8 +456,8 @@ def test_force_close_channel_onchain_layer_one_layer_above():
 
 def test_close_channel():
     test_coop_close_channel_first_virtual_layer_no_layer_above()
-    test_coop_close_channel_first_virtual_layer_one_layer_above_first_direction()
-    test_coop_close_channel_first_virtual_layer_one_layer_above_second_direction()
+    test_coop_close_channel_first_virtual_layer_one_layer_above(forward = True)
+    test_coop_close_channel_first_virtual_layer_one_layer_above(forward = False)
     test_force_close_channel_onchain_layer_one_layer_above()
 
 def test_force_close1():
