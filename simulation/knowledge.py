998a09daefa50d5df1c70124fa71be3ac9e89444
uscpr <michael.schaller@tutanota.de>
2022-07-18 10:36:42 +0200

    add some more knowledge modes and functions

--- simulation/knowledge.py
+++ simulation/knowledge.py
@@ -2,6 +2,13 @@
 def know_all_function(party, payments):
     return payments
 
+def know_mine_function(party, payments):
+    return [payment for payment in payments if payment[0] == party]
+
+def know_next_function(party, payments):
+    # works also for empty list
+    return payments[:1]
+
 class Knowledge:
     def __init__(self, knowledge_mode, knowledge_function = None):
         match knowledge_mode:
@@ -9,7 +16,10 @@ class Knowledge:
                 self.knowledge_function = knowledge_function
             case 'know-all':
                 self.knowledge_function = know_all_function
-            # ...
+            case 'know-mine':
+                self.knowledge_function = know_mine_function
+            case 'know_next':
+                self.knowledge_function = know_next_function
             case _:
                 raise ValueError
 
