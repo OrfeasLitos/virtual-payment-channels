2ddb7220131fab7a979c8bead249e6c62f36e142
uscpr <michael.schaller@tutanota.de>
2022-07-18 10:45:11 +0200

    add more knowledge functions

--- simulation/knowledge.py
+++ simulation/knowledge.py
@@ -5,9 +5,23 @@ def know_all_function(party, payments):
 def know_mine_function(party, payments):
     return [payment for payment in payments if payment[0] == party]
 
-def know_next_function(party, payments):
+def get_know_next_n_function(n):
     # works also for empty list
-    return payments[:1]
+    def know_next_n_function(party, payments):
+        return payments[:n]
+    return know_next_n_function
+
+know_next_function = get_know_next_n_function(1)
+know_next_10_function = get_know_next_n_function(10)
+
+def get_know_my_next_n_function(n):
+    def know_my_next_n_function(party, payments):
+        my_payments = [payment for payment in payments if payment[0] == party]
+        return my_payments[:10]
+    return know_my_next_n_function
+
+know_my_next_10_function = get_know_my_next_n_function(10)
+
 
 class Knowledge:
     def __init__(self, knowledge_mode, knowledge_function = None):
@@ -18,8 +32,12 @@ class Knowledge:
                 self.knowledge_function = know_all_function
             case 'know-mine':
                 self.knowledge_function = know_mine_function
-            case 'know_next':
+            case 'know-next':
                 self.knowledge_function = know_next_function
+            case 'know-next-10':
+                self.knowledge_function = know_next_10_function
+            case 'know-my-next-10-':
+                self.knowledge_function = know_my_next_10_function
             case _:
                 raise ValueError
 
