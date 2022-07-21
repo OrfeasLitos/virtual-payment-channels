5338902e2e2925b609881cb137389bcbf936c925
uscpr <michael.schaller@tutanota.de>
2022-07-21 11:24:30 +0200

    write get_new_virtual_channel_time and fee for lvpc

--- simulation/lcpv.py
+++ simulation/lcpv.py
@@ -3,11 +3,20 @@ from network import Network_LVPC
 
 class LVPC(Payment_Network):
     def __init__(self, nr_players, max_coins=2000000000000000, bitcoin_fee=1000000, bitcoin_delay=3600,
-    coins_for_parties="max_value", lvpc_fee = None, opening_transaction_size = 200, lvpc_delay = None):
+        coins_for_parties="max_value", lvpc_fee_intermediary = None, opening_transaction_size = 200,
+        lvpc_new_virtual_channel_delay = None
+    ):
         super().__init__(nr_players, max_coins, bitcoin_fee, bitcoin_delay, coins_for_parties)
         # TODO: find value for fee, delay.
-        self.lvpc_fee = lvpc_fee
-        self.lvpc_delay = lvpc_delay
+        self.lvpc_fee_intermediary = lvpc_fee_intermediary
+        self.lvpc_new_virtual_channel_delay = lvpc_new_virtual_channel_delay
         self.opening_transaction_size = opening_transaction_size
         # TODO: check what kind of network we need for LVPC
         self.network = Network_LVPC(nr_players)
+
+    # TODO: maybe multiply by 2 in accordance with elmo.
+    def get_new_virtual_channel_time(self):
+        return self.lvpc_new_virtual_channel_delay
+
+    def get_new_virtual_channel_fee(self):
+        return self.lvpc_fee_intermediary
