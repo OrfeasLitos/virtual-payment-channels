2258d976730cbf211cd508fb4245ce7dbd4656b6
uscpr <michael.schaller@tutanota.de>
2022-07-25 11:06:11 +0200

    remove default values in __init__ of super

--- simulation/elmo.py
+++ simulation/elmo.py
@@ -14,7 +14,6 @@ class Elmo(Custom_Elmo_LVPC_Donner):
         elmo_new_virtual_channel_delay = 1
     ):
         super().__init__(
-            "Elmo", nr_players, max_coins = 2000000000000000, bitcoin_fee = 1000000,
-            bitcoin_delay = 3600,  coins_for_parties = "max_value", fee_intermediary = 10000,
-            opening_transaction_size = 200, pay_delay = 0.05, new_virtual_channel_delay = 1
+            "Elmo", nr_players, max_coins, bitcoin_fee, bitcoin_delay,  coins_for_parties, fee_intermediary,
+            opening_transaction_size, elmo_pay_delay, elmo_new_virtual_channel_delay
         )
