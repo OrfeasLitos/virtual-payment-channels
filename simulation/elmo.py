f5541d5f9b10b3ab9d34bf2c99df822d71637601
Orfeas Litos <orfeas.litos@hotmail.com>
2022-07-28 15:33:20 +0300

    Add discussion for fee/delay/tx size parameters

--- simulation/elmo.py
+++ simulation/elmo.py
@@ -1,7 +1,17 @@
 from custom_elmo_lvpc_donner import Custom_Elmo_LVPC_Donner
 
 class Elmo(Custom_Elmo_LVPC_Donner):
-    # TODO: find reasonable value for fee_intermediary, opening_transaction_size, elmo_delay
+    # The fee is the same for all payment methods. It is paid on channel
+    # opening and is set to 20 times the LN fee. The rationale is that the
+    # intermediaries have to be compensated for keeping their coins locked.
+    # review: With current values, it is `fee_intermediary = 20*base_fee = 20000`.
+    # review: If it is easy, we should also add an equivalent to the
+    # review: proportional `ln_fee` and make it 20 times that of ln as well.
+    # review: Re delays, I believe it's safe to use the same delay for all
+    # payment methods. in LN it is multiplied by the number of hops, in all
+    # other constructions it isn't, as in all of them parties pay each other
+    # directly in their virtual channel.
+    # TODO: determine `opening_transaction_size`
     def __init__(
         self, nr_players, max_coins = 2000000000000000,
         bitcoin_fee = 1000000, bitcoin_delay = 3600, 
