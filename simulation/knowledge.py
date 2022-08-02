5c375df32762707b052d76ca45886203c7d71d08
uscpr <michael.schaller@tutanota.de>
2022-08-02 10:29:21 +0200

    rename knowledge functions

--- simulation/knowledge.py
+++ simulation/knowledge.py
@@ -1,29 +1,28 @@
 import collections
 import itertools
 
-# TODO: remove function from name.
-def know_all_function(party, payments):
+def know_all(party, payments):
     return payments
 
-def know_mine_function(party, payments):
+def know_mine(party, payments):
     return collections.deque([payment for payment in payments if payment[0] == party])
 
-def get_know_next_n_function(n):
+def get_know_next_n(n):
     # works also for empty list
-    def know_next_n_function(party, payments):
+    def know_next_n(party, payments):
         return collections.deque(itertools.islice(payments, 0, n))
-    return know_next_n_function
+    return know_next_n
 
-know_next_function = get_know_next_n_function(1)
-know_next_10_function = get_know_next_n_function(10)
+know_next = get_know_next_n(1)
+know_next_10 = get_know_next_n(10)
 
-def get_know_my_next_n_function(n):
-    def know_my_next_n_function(party, payments):
+def get_know_my_next_n(n):
+    def know_my_next_n(party, payments):
         my_payments = [payment for payment in payments if payment[0] == party]
         return collections.deque(my_payments[:n])
-    return know_my_next_n_function
+    return know_my_next_n
 
-know_my_next_10_function = get_know_my_next_n_function(10)
+know_my_next_10 = get_know_my_next_n(10)
 
 
 class Knowledge:
@@ -32,15 +31,15 @@ class Knowledge:
             case 'customized':
                 self.knowledge_function = knowledge_function
             case 'know-all':
-                self.knowledge_function = know_all_function
+                self.knowledge_function = know_all
             case 'know-mine':
-                self.knowledge_function = know_mine_function
+                self.knowledge_function = know_mine
             case 'know-next':
-                self.knowledge_function = know_next_function
+                self.knowledge_function = know_next
             case 'know-next-10':
-                self.knowledge_function = know_next_10_function
+                self.knowledge_function = know_next_10
             case 'know-my-next-10':
-                self.knowledge_function = know_my_next_10_function
+                self.knowledge_function = know_my_next_10
             case _:
                 raise ValueError
 
