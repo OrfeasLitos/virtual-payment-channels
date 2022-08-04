fa4292feb4b991b1ffd3b11a86d981fd3eb9e533
uscpr <michael.schaller@tutanota.de>
2022-08-04 15:26:23 +0200

    set default values for fee_rate

--- simulation/custom_elmo_lvpc_donner.py
+++ simulation/custom_elmo_lvpc_donner.py
@@ -10,7 +10,7 @@ class Custom_Elmo_LVPC_Donner(Payment_Network):
         self, method_name, nr_players, max_coins = 2000000000000000,
         bitcoin_fee = 1000000, bitcoin_delay = 3600, 
         coins_for_parties = "max_value", base_fee = 10000,
-        fee_rate = 0, opening_transaction_size = 200,
+        fee_rate = 0.0004, opening_transaction_size = 200,
         pay_delay = 0.05, new_virtual_channel_delay = 1
     ):
         super().__init__(nr_players, max_coins, bitcoin_fee, bitcoin_delay, coins_for_parties)
