a9f3f1e77dce1fce303e1ba58dd2f24a94d999c1
uscpr <michael.schaller@tutanota.de>
2022-08-22 08:37:16 +0200

    start graph for costs

--- simulation/make_graphs.py
+++ simulation/make_graphs.py
@@ -1,30 +1,47 @@
 import pickle
+from turtle import width
 import matplotlib.pyplot as plt
 
 pickled_file_ln = open("example_results_ln.pickle", 'rb')
 payments_ln = pickle.load(pickled_file_ln)
 num_successful_payments_ln = 0
+sum_fees_ln = 0
 for i in range(len(payments_ln)):
     if payments_ln[i][0] == True:
         num_successful_payments_ln += 1
+        sum_fees_ln += payments_ln[i][2]
 
+print(sum_fees_ln)
 num_unsuccessful_payments_ln = len(payments_ln) - num_successful_payments_ln
 
 pickled_file_elmo = open("example_results_Elmo.pickle", 'rb')
 payments_elmo = pickle.load(pickled_file_elmo)
 num_successful_payments_elmo = 0
+sum_fees_elmo = 0
 for i in range(len(payments_elmo)):
     if payments_elmo[i][0] == True:
         num_successful_payments_elmo += 1
+        sum_fees_elmo += payments_elmo[i][2]
 
+print(sum_fees_elmo)
 num_unsuccessful_payments_elmo = len(payments_elmo) - num_successful_payments_elmo
 
-x_coords = [0,1,2,3]
+x_coords_success = [0,1,2,3]
 payments_bar = [
     num_successful_payments_ln, num_unsuccessful_payments_ln,
     num_successful_payments_elmo, num_unsuccessful_payments_elmo
 ]
-labels = ['successul_ln', 'unsuccessful_ln', 'successul_elmo', 'unsuccessul_elmo']
+labels_success = ['successul_ln', 'unsuccessful_ln', 'successul_elmo', 'unsuccessul_elmo']
 
-plt.bar(x_coords, payments_bar, tick_label = labels, width = 0.8, color = ['blue', 'red'])
+plt.bar(x_coords_success, payments_bar, tick_label = labels_success, width = 0.8, color = ['blue', 'red'])
+plt.show()
+
+x_coords_cost = [0, 1]
+labels_cost = ['cost-ln', 'cost-elmo']
+
+cost_bar = [
+    sum_fees_ln, sum_fees_elmo,
+]
+
+plt.bar(x_coords_cost, cost_bar, tick_label = labels_cost, width=0.5)
 plt.show()
