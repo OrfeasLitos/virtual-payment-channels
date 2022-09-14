ac4f5edf79675b034fa1bb1fdc14061addd1f640
uscpr <michael.schaller@tutanota.de>
2022-09-14 08:29:04 +0200

    remove old review about channel closing and comment regarding review

--- simulation/network.py
+++ simulation/network.py
@@ -165,15 +165,9 @@ class Custom_Network_Elmo_LVPC_Donner(Network):
             channels_below_upper_channel_C_to_D = self.graph[idC][idD]['channels_below']
             # assume that channel can occur only once in upper layer, i.e. no cycles.
             i = channels_below_upper_channel_C_to_D.index(idA)
-            # review: Can i be 0? Does the below work then?
-            # It should, because if i = 0 then channels_below_upper_channel_C_to_D[i-1] gives the last element,
-            # which shouldn't be idB, since then C -> D would be A -> B and D -> D is above A -> B.
-            # tested in test_coop_close_channel_first_virtual_layer_one_layer_above
             j = i - 1 if channels_below_upper_channel_C_to_D[i-1] == idB else i+1
             is_right_party_closing = channels_below_upper_channel_C_to_D[i-1] == idB
             j = i - 1 if is_right_party_closing else i + 1
-            # review: this is a big improvement in the logic. Has it been thoroughly tested for equivalence with the old one?
-            # tested in test_coop_close_channel_first_virtual_layer_one_layer_above
             (first_index, second_index) = (j, i) if is_right_party_closing else (i, j)
             path_length_C_to_D = len(channels_below_upper_channel_C_to_D)
             second_index_reverse = path_length_C_to_D - 1 - first_index
