f81515f0632c593f84aa40423563fa98d35a316d
uscpr <michael.schaller@tutanota.de>
2022-09-20 15:35:29 +0200

    make more payments per player in power law

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -180,17 +180,18 @@ if __name__ == "__main__":
     payments_uniform = pickle.load(pickled_file_uniform)
 
     
-    payments = payments_zipf[(100, 3., 0)]
+    payments = payments_zipf[(1000, 2., 0)]
+    print(len(payments))
     #payments = payments_uniform[(100, 1000, 0)]
     utilities = [
         #Utility('sum_of_inverses', parameters = (1000, 1000000, 1000, 10000, 0)),
         #Utility('sum_of_inverses', parameters = (1000, 1000000, 1000, 10000, 1000000)),
-        Utility('sum_of_inverses', personalization = ("50-50", 100), parameters = [(1, 1000000, 10000, 0.0001, 0), (1, 0, 10000, 0.0001, 0)])
+        Utility('sum_of_inverses', personalization = ("same-utility", 1000), parameters = [(1, 1000000, 10000, 0.0001, 0), (1, 0, 10000, 0.0001, 0)])
     ]
-    for method in [Elmo(100)]:#, Donner(100), LVPC(100)]:#, LN(1000)]:
+    for method in [Elmo(1000)]:#, Donner(100), LVPC(100)]:#, LN(1000)]:
         for utility in utilities:
             for knowledge in [
-                Knowledge('all')#, Knowledge('10-next'), Knowledge('next'),
+                Knowledge('10-next')#, Knowledge('all'), Knowledge('next'),
                 #Knowledge('mine'), Knowledge('10-next-mine')
             ]:
                 sim = Simulation(copy.copy(payments), method, knowledge, utility)
