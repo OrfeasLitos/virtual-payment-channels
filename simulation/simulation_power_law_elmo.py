7ec2850b5a43c6943904cc9c59bf80b25d7eb590
uscpr <michael.schaller@tutanota.de>
2022-09-23 15:43:27 +0200

    change output file name

--- simulation/simulation_power_law_elmo.py
+++ simulation/simulation_power_law_elmo.py
@@ -30,6 +30,6 @@ if __name__ == "__main__":
         results = sim.run()
         end = time.time()
         print("Time one round: ", end - start)
-        with open("example_results_" + method.method_name + "_{}".format(i) + ".pickle", 'wb') as file:
+        with open("results_power_law" + method.method_name + "_{}".format(i) + ".pickle", 'wb') as file:
             pickle.dump(results, file)
 
