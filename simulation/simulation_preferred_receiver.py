261acd7b159324ea8a7b9a475ff4cbb066628a82
uscpr <michael.schaller@tutanota.de>
2022-09-25 09:44:58 +0200

    code for preferred receiver

---
+++ simulation/simulation_preferred_receiver.py
@@ -0,0 +1,33 @@
+import random
+import numpy as np
+import collections
+import pickle
+from paymentmethod import MAX_COINS
+from simulation import random_payments, ROUNDS_RANDOM_PAYMENTS, SEED
+from tqdm import tqdm
+
+
+def all_random_payments_preferred_receiver():
+    # preferred receiver
+    for parties in [100]:
+        for num_payments in [10000]:
+            for i in tqdm(range(ROUNDS_RANDOM_PAYMENTS)):
+                payments = random_payments(parties, 'preferred-receiver', num_payments)
+                with open('random_payments_preferred_receiver' + '_{}_'.format(parties) + '{}'.format(i) + '.pickle', 'wb') as file:
+                    pickle.dump(payments, file)
+
+
+
+if __name__ == "__main__":
+
+    #flamegraph.start_profile_thread(fd=open("./perf_1.log", "w"))
+    #seed = random.randrange(sys.maxsize)
+    
+    # uncomment to generate random payments.
+    
+    random.seed(SEED)
+    np.random.seed(SEED)
+    all_random_payments_preferred_receiver()
+
+
+    
---
+++ simulation/simulation_preferred_receiver_donner.py
@@ -0,0 +1,34 @@
+
+import random
+import pickle
+import time
+from tqdm import tqdm
+import numpy as np
+
+from utility import Utility
+from knowledge import Knowledge
+from donner import Donner
+from simulation import Simulation, SEED, ROUNDS_RANDOM_PAYMENTS
+
+if __name__ == "__main__":
+
+    random.seed(SEED + 100)
+    np.random.seed(SEED + 100)
+
+    utility = Utility(
+        'sum_of_inverses', personalization = ("50-50", 100),
+        parameters = [(1, 100000000, 1000, 0.001, 0.1), (1, 1000000, 10000, 0.001, 0.1)]
+    )
+    knowledge = Knowledge('mine')
+    for i in tqdm(range(ROUNDS_RANDOM_PAYMENTS)):
+        method = Donner(100)
+        with open('random_payments_preferred_receiver_100' + '_{}'.format(i) + '.pickle', 'rb') as pickled_file_preferred_receiver:
+            payments_preferred_receiver = pickle.load(pickled_file_preferred_receiver)
+        print("Number payments: ", len(payments_preferred_receiver))
+        sim = Simulation(payments_preferred_receiver, method, knowledge, utility)
+        start = time.time()
+        results = sim.run()
+        end = time.time()
+        print("Time one round: ", end - start)
+        with open("results_preferred_receiver" + method.method_name + "_{}".format(i) + ".pickle", 'wb') as file:
+            pickle.dump(results, file)
