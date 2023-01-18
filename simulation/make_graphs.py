41f20c81847532e7c7f5a2f0503b740c01798838
Orfeas Litos <orfeas.litos@hotmail.com>
2023-01-18 18:05:02 +0000

    Homogenise labels

--- simulation/make_graphs.py
+++ simulation/make_graphs.py
@@ -246,7 +246,7 @@ for distribution in tqdm(["power_law", "preferred_receiver", "uniform"]):
         x_coords_cost, cost_bar, tick_label = labels_cost,
         width=0.5, color=color, bottom=cutoff_fee
     )
-    plt.ylabel("Fee (relative)")
+    #plt.ylabel("Fee (relative)")
     plt.title(distribution.replace('_', ' ').title())
     plt.grid(color='grey', linestyle='--', linewidth=1, axis='y', alpha=0.25)
     plt.savefig("Fees_" + distribution + ".png", bbox_inches="tight")
@@ -267,7 +267,7 @@ for distribution in tqdm(["power_law", "preferred_receiver", "uniform"]):
         x_coords_delay, delay_bar, tick_label = labels_delay,
         width=0.5, color=color, bottom=cutoff_delay
     )
-    plt.ylabel("Delay (secs)")
+    #plt.ylabel("Delay (secs)")
     plt.title(distribution.replace('_', ' ').title())
     plt.grid(color='grey', linestyle='--', linewidth=1, axis='y', alpha=0.5)
     plt.savefig("Delays_" + distribution + ".png")
